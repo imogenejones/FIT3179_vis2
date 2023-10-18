187883fbb3eec19efe036a64c620331fa11aa876
imogenejones <147306578+imogenejones@users.noreply.github.com>
2023-10-18 12:33:29 +1100

    Add files via upload

--- propbar.js
+++ propbar.js
@@ -1,4 +1,4 @@
-var spec2 = "propbarincident.vg.json";
+var spec2 = "stackedbar.vg.json";
   vegaEmbed('#propbar', spec2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
